6aef08eab02103e2368e190bfea9bed07426818b
kangbo1229 <kangbo@ssfs.org>
2019-11-12 14:16:07 -0500

    w

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
 
 public class WhileLoop {
+
     static Scanner scan = new Scanner(System.in);
+
     public static String fromHereToThere(int x, int y) {
         String num = "";
         while (x<y) {
             num +=x+" ";
             x++;
         }
+        if (x>y) {
+            return "Invalid input";
+        }
         return num;
 
     }
-    public static String factors(int a) {
 
+    public static String factors(int a){
+        int num = 12;
+        System.out.println("factors("+num+")->")
     }
+    public static void main(String [] args) {
+        System.out.println(fromHereToThere();
+    }
+
 
 }
