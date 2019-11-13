993f1b78261836ea21a763df68f95b0c150cfc71
kangbo1229 <kangbo@ssfs.org>
2019-11-13 14:46:16 -0500

    w

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -6,23 +6,53 @@ public class WhileLoop {
 
     public static String fromHereToThere(int x, int y) {
         String num = "";
-        while (x<y) {
+        while (x<=y) {
+            if (x>y) {
+                return "Invalid input";
+        }
             num +=x+" ";
             x++;
         }
-        if (x>y) {
-            return "Invalid input";
-        }
+
         return num;
 
     }
 
-    public static String factors(int a){
+    public static int factors(int a) {
         int num = 12;
-        System.out.println("factors("+num+")->")
+        System.out.println("factors(" + num + ")->The factors of " + num + " are:");
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        return num;
+    }
+    public static String CountPosAndNeg() {
+        System.out.print("Enter a positive or negative number or 0 to quit: ");
+        int num = scan.nextInt();
+        int posCount = 0, negCount = 0;
+        while( num != 0) {
+            System.out.println(num);
+            if (num>0) {
+                posCount++;
+            }
+            else if (num<0) {
+                negCount++;
+            }
+            num = scan.nextInt();
+        }
+        return "There were" + posCount + "positive and" + negCount + "negative numbers.";
+
+    }
+    public static int findMinAndMax(int num) {
+        int max = num = 0;
+
     }
     public static void main(String [] args) {
-        System.out.println(fromHereToThere();
+        System.out.println(fromHereToThere(20,30));
+        System.out.println(factors(12));
+        CountPosAndNeg();
     }
 
 
