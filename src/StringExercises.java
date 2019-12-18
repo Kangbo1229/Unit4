423c2cdae6fd6c7eb70413671401f1ecdeb6d4b2
kangbo@ssfs.org <Elly122913!>
2019-12-18 12:55:44 -0500

    Completed String Exercises

--- src/StringExercises.java
+++ src/StringExercises.java
@@ -20,6 +20,7 @@ public class StringExercises {
     public static int countTheVowels(String s){
         int L = s.length();
         int count =0;
+        s = s.toLowerCase();
         for (int i=0; i<L; i++){
             String w = s.substring(i,i+1);
             if (w.equals("i")||w.equals("e")||w.equals("o")||w.equals("u")||w.equals("a")){
@@ -33,30 +34,39 @@ public class StringExercises {
         int w = s2.length();
         int n = 0;
         if (s1.compareTo(s2) == 0) {
+            return s1 + " and " + s2 + " are the same";
+        }
+        if (L > w){
             for (int i = 0; 0 < w; i++) {
                 if (s1.substring(i, i + 1).equals(s2.substring(i, i + 1)) == false) {
                     n = i + 1;
                 }
             }
-            if (n == 0) {
+            if (n == 0)
                 n = w + 1;
-            } else if (L < w) {
+            }
+            else if (L < w) {
                 for (int I = 0; I < w; I++) {
                     if (s1.substring(I, I + 1).equals(s2.substring(I, I + 1)) == false) {
                         n = I + 1;
                     }
                 }
-                if (n == 0) {
+                if (n == 0)
                     n = w + 1;
-                } else {
-
                 }
+                else
+                    for (int I = 0; I < w; I++) {
+                        if (s1.substring(I, I + 1).equals(s2.substring(I, I + 1)) == false) {
+                            n = I + 1;
             }
         }
         return s1 + " and " + s2 + " are not the same. They differ at letter number " + n;
     }
     public static void main(String[] args){
-        System.out.println(printTheLetters("pizza"));
+        //System.out.println(printTheLetters("pizza"));
+        //System.out.println(everyOtherLetter("Computer Science"));
+        //System.out.println(countTheVowels("COMPUTER science"));
+        System.out.println(differentStrings("aardvark", "aardvark"));
     }
 
 
--- src/forLoop.java
+++ src/forLoop.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class forLoop {
-    Scanner scan = new Scanner(System.in);
 
-    public static void printingHashTags() {
+    public static String printingHashTags() {
         for (int i = 0; i < 7; i++)
             System.out.print("#");
+        return "";
     }
+
 
         public static void countdown() {
             for (int x = 10; x >= 0; x -= 1)
@@ -18,10 +19,7 @@ public class forLoop {
 
         }
         public static void main(String args[]){
-            //System.out.print("printingHashTags(7)-> ");
-            //printingHashTags();
-            //System.out.print("countDown(0,10)-> ");
-            //countdown();
+            System.out.println(printingHashTags());
 
         }
 }
