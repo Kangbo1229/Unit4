d7fc8107c615d0fa0a5c286a5bac1232c7716c74
kangbo1229 <kangbo@ssfs.org>
2019-12-18 13:02:28 -0500

    Completed Forloop

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,5 +1,5 @@
 public class NestedLoops {
-    public static String xSquare(int num) {
+    public static String xSquare(int n) {
         String blank = "";
         for (int x=1; x <=5; x++) {
             for (int y=1; y <= 5; y++)
@@ -7,7 +7,36 @@ public class NestedLoops {
         }
         return blank;
     }
+    public static String xSquare2(int n) {
+        String blank = "";
+        for (int x=1; x <=8; x++) {
+            for (int y=1; y <=8; y++)
+                blank += "X";
+        }
+        return blank;
+    }
+    public static String flippedTriangle (int n) {
+        String blank = "";
+        for (int x = 6; x <= 1; x++) {
+            for (int y = 6; y <= x; y++)
+                blank += "*";
+        }
+        return blank;
+    }
+    public static String multiplicationTable (int n) {
+        int a;
+        int b;
+        for (int x = 1; x <=9; x++) {
+            for (int y = 1; x<= 9; y++){
+                System.out.print(" ");
+            }
+        }
+        return "";
+    }
     public static void main (String[] args) {
-        System.out.print(xSquare(5));
+       // System.out.print(xSquare(5));
+       // System.out.print(xSquare2(8));
+        System.out.print(flippedTriangle(6));
+        System.out.print(multiplicationTable(1));
     }
 }
